de5f709510051825dcace4b90b92a7a2dbe0a8c8
Grin <tyler.jefferies117@gmail.com>
2023-05-23 18:57:01 +1000

    Code Clean up

--- src/main/java/net/grinner117/forgottenmobs/entity/custom/AnimatedIronArmorEntity.java
+++ src/main/java/net/grinner117/forgottenmobs/entity/custom/AnimatedIronArmorEntity.java
@@ -1,38 +1,18 @@
 package net.grinner117.forgottenmobs.entity.custom;
 
-import net.minecraft.core.BlockPos;
-import net.minecraft.core.particles.ParticleTypes;
-import net.minecraft.sounds.SoundEvent;
-import net.minecraft.sounds.SoundEvents;
-import net.minecraft.world.damagesource.DamageSource;
+import net.grinner117.forgottenmobs.entity.type.AnimatedArmor;
 import net.minecraft.world.entity.EntityType;
 import net.minecraft.world.entity.ai.attributes.AttributeSupplier;
 import net.minecraft.world.entity.ai.attributes.Attributes;
-import net.minecraft.world.entity.ai.goal.FloatGoal;
-import net.minecraft.world.entity.ai.goal.LookAtPlayerGoal;
-import net.minecraft.world.entity.ai.goal.MeleeAttackGoal;
-import net.minecraft.world.entity.ai.goal.target.HurtByTargetGoal;
-import net.minecraft.world.entity.ai.goal.target.NearestAttackableTargetGoal;
 import net.minecraft.world.entity.monster.Monster;
-import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.level.Level;
-import net.minecraft.world.level.block.state.BlockState;
-import software.bernie.geckolib3.core.AnimationState;
-import software.bernie.geckolib3.core.IAnimatable;
-import software.bernie.geckolib3.core.PlayState;
-import software.bernie.geckolib3.core.builder.AnimationBuilder;
-import software.bernie.geckolib3.core.controller.AnimationController;
-import software.bernie.geckolib3.core.event.predicate.AnimationEvent;
-import software.bernie.geckolib3.core.manager.AnimationData;
-import software.bernie.geckolib3.core.manager.AnimationFactory;
-import software.bernie.geckolib3.util.GeckoLibUtil;
 
-public class AnimatedIronArmorEntity extends Monster implements IAnimatable {
-    AnimationFactory manager = GeckoLibUtil.createFactory(this);
+public class AnimatedIronArmorEntity extends AnimatedArmor {
     public AnimatedIronArmorEntity(EntityType<? extends Monster> EntityType, Level Level) {
         super(EntityType, Level);
         this.xpReward = 60;
     }
+
     public static AttributeSupplier setAttributes() {
         return Monster.createMonsterAttributes()
                 .add(Attributes.MAX_HEALTH, 11.0D)
@@ -45,93 +25,4 @@ public class AnimatedIronArmorEntity extends Monster implements IAnimatable {
                 .add(Attributes.KNOCKBACK_RESISTANCE, 1.0D)
                 .build();
     }
-
-
-    public void aiStep() {
-        if (this.level.isClientSide) {
-            for(int i = 0; i < 2; ++i) {
-                this.level.addParticle(ParticleTypes.PORTAL, this.getRandomX(0.3D), this.getRandomY() - 0.23D, this.getRandomZ(0.3D), (this.random.nextDouble() - 0.5D) * 2.0D, -this.random.nextDouble(), (this.random.nextDouble() - 0.5D) * 2.0D);
-            }
-        }
-        super.aiStep();
-    }
-
-    @Override
-    protected void registerGoals() {
-        this.goalSelector.addGoal(2, new MeleeAttackGoal(this, 0.25F, false));
-        this.goalSelector.addGoal(3, new LookAtPlayerGoal(this, Player.class, 64.0F));
-
-
-        this.targetSelector.addGoal(1, new NearestAttackableTargetGoal<>(this, Player.class, true));
-        this.targetSelector.addGoal(2, new HurtByTargetGoal(this));
-    }
-    protected void playStepSound(BlockPos p_34316_, BlockState p_34317_) {
-        this.playSound(SoundEvents.WITHER_SKELETON_STEP, 1.0F, 0.5F);
-    }
-    protected SoundEvent getHurtSound(DamageSource p_32527_) {
-        return SoundEvents.ANVIL_BREAK;
-    }
-    protected SoundEvent getDeathSound() {
-        return SoundEvents.ENDERMAN_DEATH;
-    }
-
-    protected SoundEvent getStepSound() {
-        return SoundEvents.ARMOR_EQUIP_CHAIN;
-    }
-
-    protected float getSoundVolume() {
-        return 1.0F;
-    }
-
-
-    //immune to potion effects
-    @Override
-    public boolean isAffectedByPotions() {
-        return false;
-    }
-    public boolean isFireImmune() {
-        return true;
-    }
-
-    public boolean isSensitiveToWater() {
-        return false;
-    }
-    @Override
-    public boolean canBreatheUnderwater() {
-        return true;}
-
-
-
-
-    private <E extends IAnimatable> PlayState predicate(AnimationEvent<E> event) {
-        if (event.isMoving()) {
-            event.getController().setAnimation(new AnimationBuilder().addAnimation("animation.animatedarmor.walk", true));
-            return PlayState.CONTINUE;
-        }
-        event.getController().setAnimation(new AnimationBuilder().addAnimation("animation.animatedarmor.idle", true));
-        return PlayState.CONTINUE;
-    }
-
-    private PlayState attackPredicate(AnimationEvent event) {
-        if (this.swinging && event.getController().getAnimationState().equals(AnimationState.Stopped)) {
-            event.getController().markNeedsReload();
-            event.getController().setAnimation(new AnimationBuilder().addAnimation("animation.animatedarmor.attack", false));
-            this.swinging = false;
-        }
-        return PlayState.CONTINUE;
-    }
-
-    @Override
-    public void registerControllers(AnimationData data) {
-        data.addAnimationController(new AnimationController(this, "controller",
-                0, this::predicate));
-        data.addAnimationController(new AnimationController(this, "attackController",
-                0, this::attackPredicate));
-    }
-    @Override
-    public AnimationFactory getFactory() {
-        return manager;
-    }
-
 }
-
