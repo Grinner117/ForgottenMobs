fe6acd8e602c8dcbac156b5a8bf361402fc581df
Grin <tyler.jefferies117@gmail.com>
2023-07-20 20:52:32 +1000

    nbh

---
+++ src/main/java/net/grinner117/forgottenmobs/entity/type/Goblin.java
@@ -0,0 +1,11 @@
+package net.grinner117.forgottenmobs.entity.type;
+
+import net.minecraft.world.entity.EntityType;
+import net.minecraft.world.entity.PathfinderMob;
+import net.minecraft.world.level.Level;
+
+public class Goblin extends PathfinderMob {
+	protected Goblin(EntityType<? extends PathfinderMob> pEntityType, Level pLevel) {
+		super(pEntityType, pLevel);
+	}
+}
