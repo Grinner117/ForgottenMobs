2d803751933922875d8b522fb3cb83ae2c992cbc
Grinner117 <tyler.jefferies117@gmail.com>
2023-07-27 23:39:45 +1000

    wwip

--- src/main/java/net/grinner117/forgottenmobs/entity/event/ModEvents.java
+++ src/main/java/net/grinner117/forgottenmobs/entity/event/ModEvents.java
@@ -22,12 +22,7 @@ public class ModEvents {
         @SubscribeEvent
         public static void entityAttributeEvent(EntityAttributeCreationEvent event) {
 
-            event.put(ModEntityTypes.ANIMATEDIRONARMOR.get(), AnimatedIronArmorEntity.setAttributes());
-            event.put(ModEntityTypes.ANIMATEDLEATHERARMOR.get(), AnimatedLeatherArmorEntity.setAttributes());
-            event.put(ModEntityTypes.ANIMATEDGOLDARMOR.get(), AnimatedGoldArmorEntity.setAttributes());
-            event.put(ModEntityTypes.ANIMATEDDIAMONDARMOR.get(), AnimatedDiamondArmorEntity.setAttributes());
-
-            event.put(ModEntityTypes.SHADOWWYRMLING.get(), ShadowWyrmlingEntity.setAttributes());
+                  event.put(ModEntityTypes.SHADOWWYRMLING.get(), ShadowWyrmlingEntity.setAttributes());
             event.put(ModEntityTypes.SHADOWYOUNGDRAGON.get(), ShadowYoungDragonEntity.setAttributes());
             event.put(ModEntityTypes.SHADOWADULTDRAGON.get(), ShadowAdultDragonEntity.setAttributes());
             event.put(ModEntityTypes.SHADOWELDERDRAGON.get(), ShadowElderDragonEntity.setAttributes());
@@ -37,38 +32,6 @@ public class ModEvents {
             event.put(ModEntityTypes.WHITEADULTDRAGON.get(), WhiteAdultDragonEntity.setAttributes());
             event.put(ModEntityTypes.WHITEELDERDRAGON.get(), WhiteElderDragonEntity.setAttributes());
 
-            event.put(ModEntityTypes.GOBLINFIGHTER.get(), GoblinFighterEntity.setAttributes());
-            event.put(ModEntityTypes.GOBLINARCHER.get(), GoblinArcherEntity.setAttributes());
-            event.put(ModEntityTypes.GOBLINSHAMAN.get(), GoblinShamanEntity.setAttributes());
-
-
-            event.put(ModEntityTypes.BEHOLDER71.get(), Beholder71Entity.setAttributes());
-            event.put(ModEntityTypes.BEHOLDER72.get(), Beholder72Entity.setAttributes());
-
-            event.put(ModEntityTypes.GREENHAG.get(), GreenHagEntity.setAttributes());
-
-            event.put(ModEntityTypes.OWLBEARFOREST.get(), OwlBearForestEntity.setAttributes());
-            event.put(ModEntityTypes.OWLBEARSNOW.get(), OwlBearSnowEntity.setAttributes());
-
-            event.put(ModEntityTypes.NEEDLEBLIGHT.get(), NeedleBlightEntity.setAttributes());
-            event.put(ModEntityTypes.TWIGBLIGHT.get(), TwigBlightEntity.setAttributes());
-            event.put(ModEntityTypes.VINEBLIGHT.get(), VineBlightEntity.setAttributes());
-
-
-            event.put(ModEntityTypes.UNICORN.get(), UnicornEntity.setAttributes());
-            event.put(ModEntityTypes.GRIFFON.get(), GriffonEntity.setAttributes());
-            event.put(ModEntityTypes.COUATL.get(), CouatlEntity.setAttributes());
-            event.put(ModEntityTypes.INTELLECTDEVOURER.get(), IntellectDevourerEntity.setAttributes());
-            event.put(ModEntityTypes.CLOUDGIANT.get(), CloudGiantEntity.setAttributes());
-            event.put(ModEntityTypes.GHOST.get(), GhostEntity.setAttributes());
-            event.put(ModEntityTypes.MINDFLAYER.get(), MindflayerEntity.setAttributes());
-            event.put(ModEntityTypes.DBEAST.get(), DBeastEntity.setAttributes());
-            event.put(ModEntityTypes.IDBEAST.get(), IDBeastEntity.setAttributes());
-            event.put(ModEntityTypes.GRIMLOCK.get(), GrimlockEntity.setAttributes());
-            event.put(ModEntityTypes.SHAMBLINGMOUND.get(), ShamblingMoundEntity.setAttributes());
-
-
-
-        }
+                   }
     }
 }
