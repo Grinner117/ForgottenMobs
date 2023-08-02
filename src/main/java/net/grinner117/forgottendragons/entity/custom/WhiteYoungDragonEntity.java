5d8905fed1cf14dc965cbe2499cb1b4c8932044b
Grinner117 <tyler.jefferies117@gmail.com>
2023-08-02 21:45:09 +1000

    WIP

--- src/main/java/net/grinner117/forgottendragons/entity/custom/WhiteYoungDragonEntity.java
+++ src/main/java/net/grinner117/forgottendragons/entity/custom/WhiteYoungDragonEntity.java
@@ -1,6 +1,7 @@
 package net.grinner117.forgottendragons.entity.custom;
 
 import net.grinner117.forgottendragons.entity.type.WhiteDragon;
+import net.grinner117.forgottenmobs.entity.type.Dragon;
 import net.minecraft.world.entity.EntityType;
 import net.minecraft.world.entity.ai.attributes.AttributeSupplier;
 import net.minecraft.world.entity.ai.attributes.Attributes;
@@ -8,10 +9,12 @@ import net.minecraft.world.entity.monster.Monster;
 import net.minecraft.world.level.Level;
 
 public class WhiteYoungDragonEntity extends WhiteDragon {
-    public WhiteYoungDragonEntity(EntityType<? extends WhiteYoungDragonEntity> p_33101_, Level p_33102_) {
+
+    public WhiteYoungDragonEntity(EntityType<? extends Dragon> p_33101_, Level p_33102_) {
         super(p_33101_, p_33102_);
         this.xpReward = 110;
     }
+
     public static AttributeSupplier setAttributes() {
         return Monster.createMonsterAttributes()
                 .add(Attributes.MAX_HEALTH, 120.0D)
