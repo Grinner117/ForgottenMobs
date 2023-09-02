6d2a5e924065742d28511ef1dc46133da50e5642
Grin <tyler.jefferies117@gmail.com>
2023-09-02 15:58:42 +1000

    Perk Update

--- src/main/java/net/grinner117/forgottendragons/item/ModItems.java
+++ src/main/java/net/grinner117/forgottendragons/item/ModItems.java
@@ -3,7 +3,7 @@ package net.grinner117.forgottendragons.item;
 import net.grinner117.forgottendragons.base.ModArmorMaterial;
 import net.grinner117.forgottendragons.entity.ModEntityTypes;
 import net.grinner117.forgottendragons.ForgottenDragons;
-import net.grinner117.forgottendragons.perk.*;
+import net.grinner117.forgottenmobs.perk.*;
 import net.grinner117.forgottenmobs.item.ModCreativeModeTab;
 import net.minecraft.sounds.SoundEvents;
 import net.minecraft.world.entity.EquipmentSlot;
@@ -16,127 +16,129 @@ import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.registries.DeferredRegister;
 import net.minecraftforge.registries.ForgeRegistries;
 import net.minecraftforge.registries.RegistryObject;
+
 public class ModItems {
-    public static final DeferredRegister<Item> ITEMS =
-            DeferredRegister.create(ForgeRegistries.ITEMS, ForgottenDragons.MODID);
+	public static final DeferredRegister<Item> ITEMS =
+			DeferredRegister.create(ForgeRegistries.ITEMS, ForgottenDragons.MODID);
 
-    public static final RegistryObject<Item> SHADOWWYRMLING_SPAWN_EGG = ITEMS.register("shadowwyrmling_spawn_egg",
-            () -> new ForgeSpawnEggItem(net.grinner117.forgottendragons.entity.ModEntityTypes.SHADOWWYRMLING, 0x1eff3f, 0x000000,
-                    new Item.Properties().tab(ModCreativeModeTab.FORGOTTENTAB)));
-    public static final RegistryObject<Item> SHADOWYOUNGDRAGON_SPAWN_EGG = ITEMS.register("shadowyoungdragon_spawn_egg",
-            () -> new ForgeSpawnEggItem(net.grinner117.forgottendragons.entity.ModEntityTypes.SHADOWYOUNGDRAGON, 0x1eff3f, 0x000000,
-                    new Item.Properties().tab(ModCreativeModeTab.FORGOTTENTAB)));
-    public static final RegistryObject<Item> SHADOWADULTDRAGON_SPAWN_EGG = ITEMS.register("shadowadultdragon_spawn_egg",
-            () -> new ForgeSpawnEggItem(net.grinner117.forgottendragons.entity.ModEntityTypes.SHADOWADULTDRAGON, 0x1eff3f, 0x000000,
-                    new Item.Properties().tab(ModCreativeModeTab.FORGOTTENTAB)));
-    public static final RegistryObject<Item> SHADOWELDERDRAGON_SPAWN_EGG = ITEMS.register("shadowelderdragon_spawn_egg",
-            () -> new ForgeSpawnEggItem(net.grinner117.forgottendragons.entity.ModEntityTypes.SHADOWELDERDRAGON, 0x1eff3f, 0x000000,
-                    new Item.Properties().tab(ModCreativeModeTab.FORGOTTENTAB)));
-    public static final RegistryObject<Item> WHITEWYRMLING_SPAWN_EGG = ITEMS.register("whitewyrmling_spawn_egg",
-            () -> new ForgeSpawnEggItem(net.grinner117.forgottendragons.entity.ModEntityTypes.WHITEWYRMLING, 0x1eff3f, 0x000000,
-                    new Item.Properties().tab(ModCreativeModeTab.FORGOTTENTAB)));
-    public static final RegistryObject<Item> WHITEYOUNGDRAGON_SPAWN_EGG = ITEMS.register("whiteyoungdragon_spawn_egg",
-            () -> new ForgeSpawnEggItem(net.grinner117.forgottendragons.entity.ModEntityTypes.WHITEYOUNGDRAGON, 0x1eff3f, 0x000000,
-                    new Item.Properties().tab(ModCreativeModeTab.FORGOTTENTAB)));
-    public static final RegistryObject<Item> WHITEADULTDRAGON_SPAWN_EGG = ITEMS.register("whiteadultdragon_spawn_egg",
-            () -> new ForgeSpawnEggItem(net.grinner117.forgottendragons.entity.ModEntityTypes.WHITEADULTDRAGON, 0x1eff3f, 0x000000,
-                    new Item.Properties().tab(ModCreativeModeTab.FORGOTTENTAB)));
-    public static final RegistryObject<Item> WHITEELDERDRAGON_SPAWN_EGG = ITEMS.register("whiteelderdragon_spawn_egg",
-            () -> new ForgeSpawnEggItem(ModEntityTypes.WHITEELDERDRAGON, 0x1eff3f, 0x000000,
-                    new Item.Properties().tab(ModCreativeModeTab.FORGOTTENTAB)));
+	public static final RegistryObject<Item> SHADOWWYRMLING_SPAWN_EGG = ITEMS.register("shadowwyrmling_spawn_egg",
+			() -> new ForgeSpawnEggItem(net.grinner117.forgottendragons.entity.ModEntityTypes.SHADOWWYRMLING, 0x1eff3f, 0x000000,
+					new Item.Properties().tab(ModCreativeModeTab.FORGOTTENTAB)));
+	public static final RegistryObject<Item> SHADOWYOUNGDRAGON_SPAWN_EGG = ITEMS.register("shadowyoungdragon_spawn_egg",
+			() -> new ForgeSpawnEggItem(net.grinner117.forgottendragons.entity.ModEntityTypes.SHADOWYOUNGDRAGON, 0x1eff3f, 0x000000,
+					new Item.Properties().tab(ModCreativeModeTab.FORGOTTENTAB)));
+	public static final RegistryObject<Item> SHADOWADULTDRAGON_SPAWN_EGG = ITEMS.register("shadowadultdragon_spawn_egg",
+			() -> new ForgeSpawnEggItem(net.grinner117.forgottendragons.entity.ModEntityTypes.SHADOWADULTDRAGON, 0x1eff3f, 0x000000,
+					new Item.Properties().tab(ModCreativeModeTab.FORGOTTENTAB)));
+	public static final RegistryObject<Item> SHADOWELDERDRAGON_SPAWN_EGG = ITEMS.register("shadowelderdragon_spawn_egg",
+			() -> new ForgeSpawnEggItem(net.grinner117.forgottendragons.entity.ModEntityTypes.SHADOWELDERDRAGON, 0x1eff3f, 0x000000,
+					new Item.Properties().tab(ModCreativeModeTab.FORGOTTENTAB)));
+	public static final RegistryObject<Item> WHITEWYRMLING_SPAWN_EGG = ITEMS.register("whitewyrmling_spawn_egg",
+			() -> new ForgeSpawnEggItem(net.grinner117.forgottendragons.entity.ModEntityTypes.WHITEWYRMLING, 0x1eff3f, 0x000000,
+					new Item.Properties().tab(ModCreativeModeTab.FORGOTTENTAB)));
+	public static final RegistryObject<Item> WHITEYOUNGDRAGON_SPAWN_EGG = ITEMS.register("whiteyoungdragon_spawn_egg",
+			() -> new ForgeSpawnEggItem(net.grinner117.forgottendragons.entity.ModEntityTypes.WHITEYOUNGDRAGON, 0x1eff3f, 0x000000,
+					new Item.Properties().tab(ModCreativeModeTab.FORGOTTENTAB)));
+	public static final RegistryObject<Item> WHITEADULTDRAGON_SPAWN_EGG = ITEMS.register("whiteadultdragon_spawn_egg",
+			() -> new ForgeSpawnEggItem(net.grinner117.forgottendragons.entity.ModEntityTypes.WHITEADULTDRAGON, 0x1eff3f, 0x000000,
+					new Item.Properties().tab(ModCreativeModeTab.FORGOTTENTAB)));
+	public static final RegistryObject<Item> WHITEELDERDRAGON_SPAWN_EGG = ITEMS.register("whiteelderdragon_spawn_egg",
+			() -> new ForgeSpawnEggItem(ModEntityTypes.WHITEELDERDRAGON, 0x1eff3f, 0x000000,
+					new Item.Properties().tab(ModCreativeModeTab.FORGOTTENTAB)));
 
-    public static final RegistryObject<Item> FIREWYRMLING_SPAWN_EGG = ITEMS.register("firewyrmling_spawn_egg",
-            () -> new ForgeSpawnEggItem(net.grinner117.forgottendragons.entity.ModEntityTypes.FIREWYRMLING, 0xd64129, 0x000000,
-                    new Item.Properties().tab(ModCreativeModeTab.FORGOTTENTAB)));
-    public static final RegistryObject<Item> FIREYOUNGDRAGON_SPAWN_EGG = ITEMS.register("fireyoungdragon_spawn_egg",
-            () -> new ForgeSpawnEggItem(net.grinner117.forgottendragons.entity.ModEntityTypes.FIREYOUNGDRAGON, 0xd64129, 0x000000,
-                    new Item.Properties().tab(ModCreativeModeTab.FORGOTTENTAB)));
-    public static final RegistryObject<Item> FIREADULTDRAGON_SPAWN_EGG = ITEMS.register("fireadultdragon_spawn_egg",
-            () -> new ForgeSpawnEggItem(net.grinner117.forgottendragons.entity.ModEntityTypes.FIREADULTDRAGON, 0xd64129, 0x000000,
-                    new Item.Properties().tab(ModCreativeModeTab.FORGOTTENTAB)));
-    public static final RegistryObject<Item> FIREELDERDRAGON_SPAWN_EGG = ITEMS.register("fireelderdragon_spawn_egg",
-            () -> new ForgeSpawnEggItem(ModEntityTypes.FIREELDERDRAGON, 0xd64129, 0x000000,
-                    new Item.Properties().tab(ModCreativeModeTab.FORGOTTENTAB)));
-     public static final RegistryObject<Item> NEEDLE = ITEMS.register("needle",
-            () -> new Item(new Item.Properties()));
+	public static final RegistryObject<Item> FIREWYRMLING_SPAWN_EGG = ITEMS.register("firewyrmling_spawn_egg",
+			() -> new ForgeSpawnEggItem(net.grinner117.forgottendragons.entity.ModEntityTypes.FIREWYRMLING, 0xd64129, 0x000000,
+					new Item.Properties().tab(ModCreativeModeTab.FORGOTTENTAB)));
+	public static final RegistryObject<Item> FIREYOUNGDRAGON_SPAWN_EGG = ITEMS.register("fireyoungdragon_spawn_egg",
+			() -> new ForgeSpawnEggItem(net.grinner117.forgottendragons.entity.ModEntityTypes.FIREYOUNGDRAGON, 0xd64129, 0x000000,
+					new Item.Properties().tab(ModCreativeModeTab.FORGOTTENTAB)));
+	public static final RegistryObject<Item> FIREADULTDRAGON_SPAWN_EGG = ITEMS.register("fireadultdragon_spawn_egg",
+			() -> new ForgeSpawnEggItem(net.grinner117.forgottendragons.entity.ModEntityTypes.FIREADULTDRAGON, 0xd64129, 0x000000,
+					new Item.Properties().tab(ModCreativeModeTab.FORGOTTENTAB)));
+	public static final RegistryObject<Item> FIREELDERDRAGON_SPAWN_EGG = ITEMS.register("fireelderdragon_spawn_egg",
+			() -> new ForgeSpawnEggItem(ModEntityTypes.FIREELDERDRAGON, 0xd64129, 0x000000,
+					new Item.Properties().tab(ModCreativeModeTab.FORGOTTENTAB)));
+	public static final RegistryObject<Item> NEEDLE = ITEMS.register("needle",
+			() -> new Item(new Item.Properties()));
 
-    public static final RegistryObject<Item> WHITE_DRAGON_SOUL = ITEMS.register("white_dragon_soul",
-            () -> new Item(new Item.Properties().tab(ModCreativeModeTab.FORGOTTENTAB)));
-    public static final RegistryObject<Item> SHADOW_DRAGON_SOUL = ITEMS.register("shadow_dragon_soul",
-            () -> new Item(new Item.Properties().tab(ModCreativeModeTab.FORGOTTENTAB)));
-    public static final RegistryObject<Item> FIRE_DRAGON_SOUL = ITEMS.register("fire_dragon_soul",
-            () -> new Item(new Item.Properties().tab(ModCreativeModeTab.FORGOTTENTAB)));
-    public static final RegistryObject<Item> WHITE_DRAGON_SCALES = ITEMS.register("white_dragon_scales",
-            () -> new Item(new Item.Properties().tab(ModCreativeModeTab.FORGOTTENTAB)));
-    public static final RegistryObject<Item> SHADOW_DRAGON_SCALES = ITEMS.register("shadow_dragon_scales",
-            () -> new Item(new Item.Properties().tab(ModCreativeModeTab.FORGOTTENTAB)));
-    public static final RegistryObject<Item> FIRE_DRAGON_SCALES = ITEMS.register("fire_dragon_scales",
-            () -> new Item(new Item.Properties().tab(ModCreativeModeTab.FORGOTTENTAB)));
+	public static final RegistryObject<Item> WHITE_DRAGON_SOUL = ITEMS.register("white_dragon_soul",
+			() -> new Item(new Item.Properties().tab(ModCreativeModeTab.FORGOTTENTAB)));
+	public static final RegistryObject<Item> SHADOW_DRAGON_SOUL = ITEMS.register("shadow_dragon_soul",
+			() -> new Item(new Item.Properties().tab(ModCreativeModeTab.FORGOTTENTAB)));
+	public static final RegistryObject<Item> FIRE_DRAGON_SOUL = ITEMS.register("fire_dragon_soul",
+			() -> new Item(new Item.Properties().tab(ModCreativeModeTab.FORGOTTENTAB)));
+	public static final RegistryObject<Item> WHITE_DRAGON_SCALES = ITEMS.register("white_dragon_scales",
+			() -> new Item(new Item.Properties().tab(ModCreativeModeTab.FORGOTTENTAB)));
+	public static final RegistryObject<Item> SHADOW_DRAGON_SCALES = ITEMS.register("shadow_dragon_scales",
+			() -> new Item(new Item.Properties().tab(ModCreativeModeTab.FORGOTTENTAB)));
+	public static final RegistryObject<Item> FIRE_DRAGON_SCALES = ITEMS.register("fire_dragon_scales",
+			() -> new Item(new Item.Properties().tab(ModCreativeModeTab.FORGOTTENTAB)));
 
-    public static final RegistryObject<ArmorItem> SHADOWDRAGONARMOR_HELMET = ITEMS.register("shadowdragonarmor_helmet",
-            () -> new Nightvison_Perk(ArmorTiers.SHADOWDRAGONARMOR, EquipmentSlot.HEAD, props()));
-    public static final RegistryObject<ArmorItem> SHADOWDRAGONARMOR_CHESTPLATE = ITEMS.register("shadowdragonarmor_chestplate",
-            () -> new DamageRes2_Perk(ArmorTiers.SHADOWDRAGONARMOR, EquipmentSlot.CHEST, props()));
-    public static final RegistryObject<ArmorItem> SHADOWDRAGONARMOR_LEGGINGS = ITEMS.register("shadowdragonarmor_leggings",
-            () -> new SlowFalling_Perk(ArmorTiers.SHADOWDRAGONARMOR, EquipmentSlot.LEGS, props()));
-    public static final RegistryObject<ArmorItem> SHADOWDRAGONARMOR_BOOTS = ITEMS.register("shadowdragonarmor_boots",
-            () -> new Damage3_Perk(ArmorTiers.SHADOWDRAGONARMOR, EquipmentSlot.FEET, props()));
+	public static final RegistryObject<ArmorItem> SHADOWDRAGONARMOR_HELMET = ITEMS.register("shadowdragonarmor_helmet",
+			() -> new Nightvison_Perk(ArmorTiers.SHADOWDRAGONARMOR, EquipmentSlot.HEAD, props()));
+	public static final RegistryObject<ArmorItem> SHADOWDRAGONARMOR_CHESTPLATE = ITEMS.register("shadowdragonarmor_chestplate",
+			() -> new DamageRes2_Perk(ArmorTiers.SHADOWDRAGONARMOR, EquipmentSlot.CHEST, props()));
+	public static final RegistryObject<ArmorItem> SHADOWDRAGONARMOR_LEGGINGS = ITEMS.register("shadowdragonarmor_leggings",
+			() -> new SlowFalling_Perk(ArmorTiers.SHADOWDRAGONARMOR, EquipmentSlot.LEGS, props()));
+	public static final RegistryObject<ArmorItem> SHADOWDRAGONARMOR_BOOTS = ITEMS.register("shadowdragonarmor_boots",
+			() -> new Damage3_Perk(ArmorTiers.SHADOWDRAGONARMOR, EquipmentSlot.FEET, props()));
 
-    public static final RegistryObject<ArmorItem> WHITEDRAGONARMOR_HELMET = ITEMS.register("whitedragonarmor_helmet",
-            () -> new Damage_Perk(ArmorTiers.WHITTEDRAGONARMOR, EquipmentSlot.HEAD, props()));
-    public static final RegistryObject<ArmorItem> WHITEDRAGONARMOR_CHESTPLATE = ITEMS.register("whitedragonarmor_chestplate",
-            () -> new Health3_Perk(ArmorTiers.WHITTEDRAGONARMOR, EquipmentSlot.CHEST, props()));
-    public static final RegistryObject<ArmorItem> WHITEDRAGONARMOR_LEGGINGS = ITEMS.register("whitedragonarmor_leggings",
-            () -> new Jump3_Perk(ArmorTiers.WHITTEDRAGONARMOR, EquipmentSlot.LEGS, props()));
-    public static final RegistryObject<ArmorItem> WHITEDRAGONARMOR_BOOTS = ITEMS.register("whitedragonarmor_boots",
-            () -> new DamageRes2_Perk(ArmorTiers.WHITTEDRAGONARMOR, EquipmentSlot.FEET, props()));
+	public static final RegistryObject<ArmorItem> WHITEDRAGONARMOR_HELMET = ITEMS.register("whitedragonarmor_helmet",
+			() -> new Damage_Perk(ArmorTiers.WHITTEDRAGONARMOR, EquipmentSlot.HEAD, props()));
+	public static final RegistryObject<ArmorItem> WHITEDRAGONARMOR_CHESTPLATE = ITEMS.register("whitedragonarmor_chestplate",
+			() -> new Health3_Perk(ArmorTiers.WHITTEDRAGONARMOR, EquipmentSlot.CHEST, props()));
+	public static final RegistryObject<ArmorItem> WHITEDRAGONARMOR_LEGGINGS = ITEMS.register("whitedragonarmor_leggings",
+			() -> new Jump3_Perk(ArmorTiers.WHITTEDRAGONARMOR, EquipmentSlot.LEGS, props()));
+	public static final RegistryObject<ArmorItem> WHITEDRAGONARMOR_BOOTS = ITEMS.register("whitedragonarmor_boots",
+			() -> new DamageRes2_Perk(ArmorTiers.WHITTEDRAGONARMOR, EquipmentSlot.FEET, props()));
 
-    public static final RegistryObject<ArmorItem> FIREDRAGONARMOR_HELMET = ITEMS.register("firedragonarmor_helmet",
-            () -> new Regeneration2_Perk(ArmorTiers.FIREDRAGONARMOR, EquipmentSlot.HEAD, props()));
-    public static final RegistryObject<ArmorItem> FIREDRAGONARMOR_CHESTPLATE = ITEMS.register("firedragonarmor_chestplate",
-            () -> new Fire_Perk(ArmorTiers.FIREDRAGONARMOR, EquipmentSlot.CHEST, props()));
-    public static final RegistryObject<ArmorItem> FIREDRAGONARMOR_LEGGINGS = ITEMS.register("firedragonarmor_leggings",
-            () -> new Speed3_Perk(ArmorTiers.FIREDRAGONARMOR, EquipmentSlot.LEGS, props()));
-    public static final RegistryObject<ArmorItem> FIREDRAGONARMOR_BOOTS = ITEMS.register("firedragonarmor_boots",
-            () -> new Damage3_Perk(ArmorTiers.FIREDRAGONARMOR, EquipmentSlot.FEET, props()));
-
-
+	public static final RegistryObject<ArmorItem> FIREDRAGONARMOR_HELMET = ITEMS.register("firedragonarmor_helmet",
+			() -> new Regeneration2_Perk(ArmorTiers.FIREDRAGONARMOR, EquipmentSlot.HEAD, props()));
+	public static final RegistryObject<ArmorItem> FIREDRAGONARMOR_CHESTPLATE = ITEMS.register("firedragonarmor_chestplate",
+			() -> new HellRunner_Perk(ArmorTiers.FIREDRAGONARMOR, EquipmentSlot.CHEST, props()));
+	public static final RegistryObject<ArmorItem> FIREDRAGONARMOR_LEGGINGS = ITEMS.register("firedragonarmor_leggings",
+			() -> new Speed3_Perk(ArmorTiers.FIREDRAGONARMOR, EquipmentSlot.LEGS, props()));
+	public static final RegistryObject<ArmorItem> FIREDRAGONARMOR_BOOTS = ITEMS.register("firedragonarmor_boots",
+			() -> new Damage3_Perk(ArmorTiers.FIREDRAGONARMOR, EquipmentSlot.FEET, props()));
 
 
-    public static void register(IEventBus eventBus) {
-        ITEMS.register(eventBus);
-    }
-    private static Item.Properties props(){return new Item.Properties().tab(ModCreativeModeTab.FORGOTTENTAB); }
+	public static void register(IEventBus eventBus) {
+		ITEMS.register(eventBus);
+	}
 
-    public static class ArmorTiers{
+	private static Item.Properties props() {
+		return new Item.Properties().tab(ModCreativeModeTab.FORGOTTENTAB);
+	}
 
-        public static final ArmorMaterial SHADOWDRAGONARMOR = new ModArmorMaterial(
-                "shadowdragonarmor",
-                2200,
-                new int[]{7,15,12,7},
-                200,
-                SoundEvents.PHANTOM_SWOOP,
-                10.0f, 0.8f,
-                () -> Ingredient.of(ModItems.SHADOW_DRAGON_SCALES.get()));
+	public static class ArmorTiers {
 
-        public static final ArmorMaterial FIREDRAGONARMOR = new ModArmorMaterial(
-                "firedragonarmor",
-                2500,
-                new int[]{7,15,12,7},
-                200,
-                SoundEvents.PHANTOM_SWOOP,
-                10.0f, 0.8f,
-                () -> Ingredient.of(ModItems.FIRE_DRAGON_SCALES.get()));
+		public static final ArmorMaterial SHADOWDRAGONARMOR = new ModArmorMaterial(
+				"shadowdragonarmor",
+				2200,
+				new int[]{7, 15, 12, 7},
+				200,
+				SoundEvents.PHANTOM_SWOOP,
+				10.0f, 0.8f,
+				() -> Ingredient.of(ModItems.SHADOW_DRAGON_SCALES.get()));
 
-        public static final ArmorMaterial WHITTEDRAGONARMOR = new ModArmorMaterial(
-                "whitedragonarmor",
-                2600,
-                new int[]{7,15,12,7},
-                200,
-                SoundEvents.GLASS_BREAK,
-                14.0f, 1.8f,
-                () -> Ingredient.of(ModItems.WHITE_DRAGON_SCALES.get()));
+		public static final ArmorMaterial FIREDRAGONARMOR = new ModArmorMaterial(
+				"firedragonarmor",
+				2500,
+				new int[]{7, 15, 12, 7},
+				200,
+				SoundEvents.PHANTOM_SWOOP,
+				10.0f, 0.8f,
+				() -> Ingredient.of(ModItems.FIRE_DRAGON_SCALES.get()));
 
-    }
+		public static final ArmorMaterial WHITTEDRAGONARMOR = new ModArmorMaterial(
+				"whitedragonarmor",
+				2600,
+				new int[]{7, 15, 12, 7},
+				200,
+				SoundEvents.GLASS_BREAK,
+				14.0f, 1.8f,
+				() -> Ingredient.of(ModItems.WHITE_DRAGON_SCALES.get()));
+
+	}
 
 }
